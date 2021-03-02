d1928d26c29434f2d8c4c9fec6b9f681dec435a1
zhadan93 <zhadan.evgenya@yandex.ru>
2021-03-02 17:56:36 +0300

    feat: implement functions min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
 exports.min = function min (array) {
-  return 0;
+    return (!array || array.length == 0) ? 0 : Math.min(...array);
 }
 
 exports.max = function max (array) {
-  return 0;
+    return (!array || array.length == 0) ? 0 : Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    return (!array || array.length == 0) ? 0 : array.reduce((sum, item) => sum + item, 0) / array.length;
 }
